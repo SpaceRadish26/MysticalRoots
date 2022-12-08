1dd9db8c0525f1ceda531144ce3dd0a7148e1eb5
Brecht <brecht.deweyer666@gmail.com>
2022-12-08 20:08:20 +0100

    Update

--- src/main/java/com/brecht/fac/item/ModItems.java
+++ src/main/java/com/brecht/fac/item/ModItems.java
@@ -4,15 +4,16 @@ import com.brecht.fac.FantasyAndCombat;
 import com.brecht.fac.block.custom.FireWoodItem;
 import com.brecht.fac.item.custom.ChopperItem;
 import com.brecht.fac.item.custom.ManMemeCoinItem;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.SwordItem;
+import com.brecht.fac.item.custom.SaberItem;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.*;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public class ModItems {
-    public static final DeferredRegister<Item>  ITEMS =
+    public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, FantasyAndCombat.MOD_ID);
 
 
@@ -21,9 +22,19 @@ public class ModItems {
 
     public static final RegistryObject<Item> FIREWOOD = ITEMS.register("firewood", () -> new FireWoodItem(new Item.Properties().tab(ModCreativeModeTab.FAC_TAB)));
 
-    public static final RegistryObject<Item> CHOPPER = ITEMS.register("chopper", () -> new ChopperItem(ModTiers.LEMON,2,3f, new Item.Properties().tab(ModCreativeModeTab.FAC_TAB).durability(400)
-            ));
-    public static final RegistryObject<Item> MAN_MEME_COIN = ITEMS.register("man_meme_coin",() -> new ManMemeCoinItem(new Item.Properties().tab(ModCreativeModeTab.FAC_TAB)));
+    public static final RegistryObject<Item> CHOPPER = ITEMS.register("chopper", () -> new ChopperItem(ModTiers.LEMON, 2, 3f, new Item.Properties().tab(ModCreativeModeTab.FAC_TAB).durability(400)
+    ));
+    public static final RegistryObject<Item> SABER = ITEMS.register("saber", () -> new SaberItem(Tiers.IRON, 3, 4f, new Item.Properties().tab(ModCreativeModeTab.FAC_TAB)));
+    public static final RegistryObject<Item> LEMON_HELMET = ITEMS.register("lemon_helmet", () -> new ArmorItem(ModArmorMaterials.LEMON, EquipmentSlot.HEAD, new Item.Properties().tab(ModCreativeModeTab.FAC_TAB)));
+
+    public static final RegistryObject<Item> LEMON_CHEST = ITEMS.register("lemon_chest", () -> new ArmorItem(ModArmorMaterials.LEMON, EquipmentSlot.CHEST, new Item.Properties().tab(ModCreativeModeTab.FAC_TAB)));
+    public static final RegistryObject<Item> LEMON_LEGGINGS = ITEMS.register("lemon_leggings", () -> new ArmorItem(ModArmorMaterials.LEMON, EquipmentSlot.LEGS, new Item.Properties().tab(ModCreativeModeTab.FAC_TAB)));
+
+    public static final RegistryObject<Item> LEMON_BOOTS = ITEMS.register("lemon_boots", () -> new ArmorItem(ModArmorMaterials.LEMON, EquipmentSlot.FEET, new Item.Properties().tab(ModCreativeModeTab.FAC_TAB)));
+
+
+    public static final RegistryObject<Item> MAN_MEME_COIN = ITEMS.register("man_meme_coin", () -> new ManMemeCoinItem(new Item.Properties().tab(ModCreativeModeTab.FAC_TAB)));
+
     public static void register(IEventBus eventbus) {
         ITEMS.register(eventbus);
     }
---
+++ src/main/java/com/brecht/fac/item/custom/SaberItem.java
@@ -0,0 +1,22 @@
+package com.brecht.fac.item.custom;
+
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.item.Tier;
+
+public class SaberItem extends SwordItem {
+
+    public SaberItem(Tier pTier, int pAttackDamageModifier, float pAttackSpeedModifier, Properties pProperties) {
+        super(pTier, pAttackDamageModifier, pAttackSpeedModifier, pProperties);
+    }
+
+    @Override
+    public boolean hurtEnemy(ItemStack pStack, LivingEntity pTarget, LivingEntity pAttacker) {
+        pTarget.addEffect(new MobEffectInstance(MobEffects.WEAKNESS,40));
+        return super.hurtEnemy(pStack, pTarget, pAttacker);
+    }
+}
